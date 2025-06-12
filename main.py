6e6c5f0d6659f440e510f70945e37cf93fb472db
schnitgurL <146375954+schnitGurl@users.noreply.github.com>
2025-06-12 14:11:06 -0400

    Updates main.py to use sys command instead of local and adds two books to repo

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from stats import get_num_words, get_char_count, sort_char_counts
+import sys # Import sys command
+from stats import get_num_words, get_char_count, sort_char_counts # Imports our needed functions
 
 def get_book_text(books):
     with open(books, 'r', encoding='utf-8') as f:
         return f.read()
 
 def main():
-    book_path = "books/frankenstein.txt"
+    ## deprecated use; book_path = "books/frankenstein.txt"
+    # Check user for correct usage
+    if len(sys.argv) !=2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    # Get file from path from command-line arg
+    book_path = sys.argv[1]
+
     book_text = get_book_text(book_path)
     word_count = get_num_words(book_text) #Get word count
     char_count = get_char_count(book_text) #Get char count
@@ -20,10 +28,10 @@ def main():
 
     print("-" * 10 + " Character Count " + "-" * 10)
 
-    # Print only alphabetical characters in descending order of frequency
+    # Print only alphabetic chars in descending order of freq
     for entry in sorted_char_list:
         char = entry["char"]
-        if char.isalpha():  # Skip non-alphabetical characters
+        if char.isalpha():  # Skip non-alphabetical chars
             print(f"{char}: {entry['num']}")
 
     print("=" * 12 + " END " + "=" * 12)
