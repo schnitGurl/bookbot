1bfba1f2ef9b7db5b0ce42f54fc74bdcacfb87fc
schnitgurL <146375954+schnitGurl@users.noreply.github.com>
2025-06-12 13:19:55 -0400

    updates stats.py with count char func

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+from stats import get_num_words, get_char_count
+
 def get_book_text(books):
     with open(books, 'r', encoding='utf-8') as f:
         return f.read()
-    
-from stats import get_num_words
 
 def main():
     book_path = "books/frankenstein.txt"
     book_text = get_book_text(book_path)
     word_count = get_num_words(book_text)
+    char_count = get_char_count(book_text)
+
     print(f"{word_count} words found in the document")
+    print(f"{char_count}")
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,3 +1,14 @@
 def get_num_words(text):
     word_count = text.split()
     return len(word_count)
+
+def get_char_count(text):
+    char_count = {}  # Dictionary to store character -> count
+
+    for char in text.lower():  # Convert to lowercase for consistency
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+
+    return char_count  # Return the dictionary of counts
